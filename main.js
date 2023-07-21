6d2b00e3fba4a3afeb91440c48202bba85583f0e
Mazenemam17 <134785119+Mazenemam17@users.noreply.github.com>
2023-07-21 23:49:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+let btn = document.getElementById("button")
+btn.onclick = function(){
+    scroll({
+        left:0,
+        top:0,
+        behavior:"smooth",
+    })
+}
+let bttn = document.getElementById("buton")
+bttn.onclick = function(){
+    scroll({
+        left:0,
+        top:0,
+        behavior:"smooth",
+    })
+}
